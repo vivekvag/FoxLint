0898eb9f0bab70367d2ff078b63977de1762a263
vivekvag <vivekvag281@gmail.com>
2025-11-16 15:52:16 +0530

    Changelog 2.3 --- Updated the UI design

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import pic from '../Pinterest.png';
+import FoxlintData from '../sample.json';
+
+function colorCode(score) {
+  if (score >= 5) return 'excellentBadge';
+  else if (score >= 4 ) return 'goodBadge';
+  else if (score >= 3 ) return 'okBadge';
+  else if (score >= 2) return 'badBadge';
+  else if (score >= 1) return 'worstBadge';
+}
+
+function printData(value) {
+  const num = Number(value)/20;
+  return parseFloat(num.toFixed(2));
+}
+
+function ScoreItem({ label, value }) {
+  const score = printData(value);
+  return (
+    <div className="rep">
+      <h4>{label}</h4>
+      <div className={colorCode(score)}>{score}/5</div>
+    </div>
+  );
+}
 
 function HomeComponent() {
   return (
@@ -11,41 +35,24 @@ function HomeComponent() {
         <div className="website">
           <img src={pic} className="App-logo" alt="logo" width={50} height={30} />
           <h5 style={{ display: 'inline-block' }}>
-            <b>pinterest.com</b>
+            <b>{FoxlintData.domain_name}</b>
           </h5>
         </div>
         <a style={{ marginLeft: 40 }} href="http://localhost:3000">
           Privacy
         </a>
       </div>
+     
       <div className="header">
         <h4 style={{ paddingTop: 10 }}>PRIVACY REPORT</h4>
       </div>
       <div className="report">
-        <div className="rep">
-          <h4>PIL Data Collected</h4>
-          <div className="successBadge">4/5</div>
-        </div>
-        <div className="rep">
-          <h4>Privacy Data Collected</h4>
-          <div className="dangerBadge">4/5</div>
-        </div>
-        <div className="rep">
-          <h4>Location Tracking</h4>
-          <div className="successBadge">4/5</div>
-        </div>
-        <div className="rep">
-          <h4>Targeted Advertising</h4>
-          <div className="okBadge">4/5</div>
-        </div>
-        <div className="rep">
-          <h4>Data Retention</h4>
-          <div className="successBadge">4/5</div>
-        </div>
-        <div className="rep">
-          <h4>Data Sharing</h4>
-          <div className="dangerBadge">4/5</div>
-        </div>
+        <ScoreItem label="PII Data Collected" value={FoxlintData.pii_data} />
+        <ScoreItem label="Privacy Data Collected" value={FoxlintData.privacy_data} />
+        <ScoreItem label="Location Tracking" value={FoxlintData.location_tracking} />
+        <ScoreItem label="Targeted Advertising" value={FoxlintData.targeted_advertising} />
+        <ScoreItem label="Data Retention" value={FoxlintData.data_retention} />
+        <ScoreItem label="Data Sharing" value={FoxlintData.data_sharing} />
       </div>
       <div className="footer" style={{ fontSize: '1.1em' }}>
         <h6>Privacy Policy was last updated on 1st March 2022</h6>
